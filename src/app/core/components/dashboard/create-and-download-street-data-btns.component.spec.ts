817d319e5afce3ebe1df23c5dd97d4b0f9e277c9
PaschalPM <okaforpaschal018@gmail.com>
2025-04-07 11:03:13 +0100

    Refactor codebase

---
+++ src/app/core/components/dashboard/create-and-download-street-data-btns.component.spec.ts
@@ -0,0 +1,24 @@
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateAndDownloadStreetDataBtnsComponent } from './create-and-download-street-data-btns.component';
+
+describe('CreateAndDownloadStreetDataBtnsComponent', () => {
+  let component: CreateAndDownloadStreetDataBtnsComponent;
+  let fixture: ComponentFixture<CreateAndDownloadStreetDataBtnsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CreateAndDownloadStreetDataBtnsComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(CreateAndDownloadStreetDataBtnsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
