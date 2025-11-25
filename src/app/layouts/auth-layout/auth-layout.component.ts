d50b94faa4ab8c78b5310638fdf3167559f054fe
Hope-olabode <olabodehope7@gmail.com>
2025-11-25 12:35:22 +0100

    changed the all rights reserved to 2025

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -13,7 +13,7 @@ import { NavbarComponent } from '../../core/components/auth/navbar/navbar.compon
       <router-outlet></router-outlet>
     </app-container>
     <footer class="py-2 text-xs text-text-color lg:text-sm text-center mt-5 font-semibold text-base-content/70">
-      © 2024, Troloppe Property Services. All Rights Reserved.
+      © 2025, Troloppe Property Services. All Rights Reserved.
     </footer>
   </div>`,
 })
